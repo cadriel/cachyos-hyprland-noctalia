c55ca93388f9c5106a45fe6f3dffacf087c0fda9
dirge <dirge@noreply.codeberg.org>
2026-06-17 12:44:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/bash
 
 # Copying configuration files into ~
-cp -i -r -v ./home/ ~/
+cp -i -r -v ./home/.config/ ~/
+cp -i -r -v ./home/.local/ ~/
+
+# Ensuring system is up to date before beginning
+sudo pacman -Syu
 
 # Installing software
-sudo pacman -Syu hyprland xdg-desktop-portal-hyprland thunar greetd nwg-hello grimblast-git nwg-look qt6ct kvantum adw-gtk-theme playerctl ristretto ttf-jetbrains-mono breeze-icons oxygen-cursors
+sudo pacman -S hyprland xdg-desktop-portal-hyprland noctalia-shell thunar greetd nwg-hello grimblast-git nwg-look qt6ct kvantum adw-gtk-theme playerctl ristretto ttf-jetbrains-mono breeze-icons oxygen-cursors
 
 # Installing some optional dependencies
 sudo pacman -S --asdeps catfish gvfs tumbler thunar-volman thunar-archive-plugin thunar-media-tags-plugin file-roller 7zip cliphist wlsunset
@@ -21,3 +25,7 @@ gsettings set org.gnome.desktop.interface cursor-theme "Oxygen_Zion"
 sudo cp -i -r -v ./etc/greetd/ /etc/
 sudo cp -i -r -v ./etc/nwg-hello/ /etc/
 systemctl enable greetd
+
+# Making the CachyOS wallpapers available to Noctalia during setup
+xdg-user-dirs-update
+ln -s /usr/share/wallpapers/cachyos-wallpapers ~/Pictures/Wallpapers
