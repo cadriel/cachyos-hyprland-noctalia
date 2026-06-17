deac4f17cfad56fea0118d2c71f5cb9edef4089c
dirge <dirge@noreply.codeberg.org>
2026-06-17 11:27:34 +0200

    Update home/.config/hypr/environment.lua

--- home/.config/hypr/environment.lua
+++ home/.config/hypr/environment.lua
@@ -5,6 +5,7 @@
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
 
 hl.env("XCURSOR_SIZE", "24")
+hl.env("XCURSOR_THEME", "Oxygen_Zion")
 hl.env("HYPRCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_THEME", "Oxygen_Zion")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
