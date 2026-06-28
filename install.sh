fee044cddd20069240fe072229401124148912b3
dirge <dirge@noreply.codeberg.org>
2026-06-29 00:52:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/bash
 
 # Copying configuration files into ~
-cp -i -r -v ./home/.config/ ~/
-cp -i -r -v ./home/.local/ ~/
-cp -i -r -v ./home/.gtkrc-2.0 ~/
+cp -i -r -v ./home/.config/ ./home/.local/ ./home/.gtkrc-2.0 ~/
 
 # Allow launching of terminal applications from Thunar
 mkdir ~/.config/xfce4
@@ -24,13 +22,12 @@ gsettings set org.gnome.desktop.interface icon-theme "breeze-dark"
 gsettings set org.gnome.desktop.interface cursor-theme "Oxygen_Zion"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 
-# Just in case the user directories weren't created
+# Just in case the XDG user directories weren't created
 xdg-user-dirs-update
 
 # Symlinking cachyos-wallpapers(if it exists) with Noctalia's default wallpaper directory
 ln -s /usr/share/wallpapers/cachyos-wallpapers ~/Pictures/Wallpapers
 
 # Configuring and enabling greetd with nwg-hello
-sudo cp -i -r -v ./etc/greetd/ /etc/
-sudo cp -i -r -v ./etc/nwg-hello/ /etc/
+sudo cp -i -r -v ./etc/greetd/ ./etc/nwg-hello/ /etc/
 systemctl enable greetd
