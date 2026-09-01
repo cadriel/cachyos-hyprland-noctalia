ba210f862e579965739a0ef20f17a71217014688
Craig Bassett <cadriel@users.noreply.github.com>
2026-09-01 13:25:25 +1200

    Refactor install.sh for package management and wallpapers
    Updated package installation and symlink creation for wallpapers.

--- install.sh
+++ install.sh
@@ -11,13 +11,15 @@ echo "TerminalEmulator=alacritty" >> ~/.config/xfce4/helpers.rc
 # Line 1: Core Packages
 # Line 2: Elective Packages
 # Line 3: Theming Packages
-sudo pacman -Syu hyprland noctalia noctalia-greeter uwsm xdg-desktop-portal-hyprland hyprpolkitagent \
-  playerctl nwg-look qt6ct kvantum \
+sudo pacman -Syu hyprland noctalia noctalia-greeter uwsm libnewt xdg-desktop-portal-hyprland \
+  pipewire wireplumber
+  playerctl nwg-look qt5ct qt6ct qt5-wayland qt6-wayland kvantum \
   thunar greetd grimblast-git ristretto mousepad \
-  adw-gtk-theme ttf-jetbrains-mono breeze-icons oxygen-cursors
+  adw-gtk-theme breeze-icons oxygen-cursors \
+  ttf-jetbrains-mono ttf-jetbrains-mono-nerd inter-font
 
 # Install Optional Dependencies
-sudo pacman -S --asdeps catfish gvfs tumbler thunar-volman thunar-archive-plugin thunar-media-tags-plugin file-roller 7zip cliphist wlsunset
+sudo pacman -S --asdeps catfish gvfs tumbler thunar-volman thunar-archive-plugin thunar-media-tags-plugin file-roller 7zip cliphist hyprsunset
 
 # Annoying GTK theming junk
 gsettings set org.gnome.desktop.interface gtk-theme "adw-gtk3-dark"
@@ -28,8 +30,9 @@ gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 # Just in case the XDG user directories weren't created
 xdg-user-dirs-update
 
-# Symlinking cachyos-wallpapers(if it exists) with Noctalia's default wallpaper directory
-ln -s /usr/share/wallpapers/cachyos-wallpapers ~/Pictures/Wallpapers
+# Set up wallpapers
+mkdir -p "$HOME/Pictures/Wallpapers"
+ln -sfn /usr/share/wallpapers/cachyos-wallpapers "$HOME/Pictures/Wallpapers/cachyos-wallpapers"
 
 # Configuring and enabling greetd with noctalia-greeter-session
 sudo sed -i '/^\[default_session\]/,/^\[/ s|^command = .*|command = "/usr/bin/noctalia-greeter-session"|' /etc/greetd/greetd.toml
